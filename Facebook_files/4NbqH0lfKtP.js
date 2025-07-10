5cdaba55085b23f4b6cde9555f71e88f196cfbab
AkosKr <akranics1@gmail.com>
2025-07-10 19:57:37 +0000

    Updates

---
+++ Facebook_files/4NbqH0lfKtP.js
@@ -0,0 +1,6 @@
+;/*FB_PKG_DELIM*/
+
+__d("StarsViewerFunnelEventFalcoEvent",["FalcoLoggerInternal","getFalcoLogPolicy_DO_NOT_USE"],(function(a,b,c,d,e,f,g){"use strict";a=c("getFalcoLogPolicy_DO_NOT_USE")("6440");b=d("FalcoLoggerInternal").create("stars_viewer_funnel_event",a);e=b;g["default"]=e}),98);
+__d("destroyOnUnload",["cr:8905"],(function(a,b,c,d,e,f,g){g["default"]=b("cr:8905")}),98);
+__d("destroyOnUnloadWWW",["Run"],(function(a,b,c,d,e,f,g){function a(a){return d("Run").onLeave(a)}g["default"]=a}),98);
+__d("getTaggingTransparencyGating",["qex"],(function(a,b,c,d,e,f,g){"use strict";function a(){var a;a=(a=c("qex")._("488"))!=null?a:!1;return a}g.getIsTaggingTransparencyAPIEnabled=a}),98);
---
+++ Facebook_files/iFJeYOsNg0n.js
@@ -0,0 +1,12 @@
+;/*FB_PKG_DELIM*/
+
+__d("FDSCautionTriangleFilled16Icon",["cr:13969"],(function(a,b,c,d,e,f,g){"use strict";g["default"]=b("cr:13969")}),98);
+__d("FDSCautionTriangleFilled16PNGIcon.react",["ix","fbicon"],(function(a,b,c,d,e,f,g,h){"use strict";a=d("fbicon")._(h("502061"),16);b=a;g["default"]=b}),98);
+__d("FDSChevronLeftFilled20Icon",["cr:14703"],(function(a,b,c,d,e,f,g){"use strict";g["default"]=b("cr:14703")}),98);
+__d("FDSChevronLeftFilled20PNGIcon.react",["ix","fbicon"],(function(a,b,c,d,e,f,g,h){"use strict";a=d("fbicon")._(h("492488"),20);b=a;g["default"]=b}),98);
+__d("FDSChevronRightFilled20Icon",["cr:14205"],(function(a,b,c,d,e,f,g){"use strict";g["default"]=b("cr:14205")}),98);
+__d("FDSChevronRightFilled20PNGIcon.react",["ix","fbicon"],(function(a,b,c,d,e,f,g,h){"use strict";a=d("fbicon")._(h("492536"),20);b=a;g["default"]=b}),98);
+__d("FDSEnvelopeFilled20Icon",["cr:13876"],(function(a,b,c,d,e,f,g){"use strict";g["default"]=b("cr:13876")}),98);
+__d("FDSEnvelopeFilled20PNGIcon.react",["ix","fbicon"],(function(a,b,c,d,e,f,g,h){"use strict";a=d("fbicon")._(h("495079"),20);b=a;g["default"]=b}),98);
+__d("FDSPlusFilled16Icon",["cr:14064"],(function(a,b,c,d,e,f,g){"use strict";g["default"]=b("cr:14064")}),98);
+__d("FDSPlusFilled16PNGIcon.react",["ix","fbicon"],(function(a,b,c,d,e,f,g,h){"use strict";a=d("fbicon")._(h("483768"),16);b=a;g["default"]=b}),98);
